03a78362d11e9ab7cdfc5240732a188634cc8c36
JhonatanRios <jhoto1234@gmail.com>
2024-09-02 17:41:58 -0500

    Ajuste menor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages.module';
 
 @Module({
   imports: [MessagesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController], // Añadir AppController aquí
+  providers: [],
 })
 export class AppModule {}
